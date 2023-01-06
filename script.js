0f17aa20e73f33671df84a1688025c3138c9869d
JonathanHaws <jonathanmhaws1@gmail.com>
2023-01-06 16:34:14 -0700

    added local storage save / load, center header, finalized other small bits

--- script.js
+++ script.js
@@ -4,8 +4,6 @@
 
 $(function () {
   
-  $("#currentDay").text(dayjs().format("dddd MMMM D")); // display current time 
-
   function createHour(hour, root){
     
     // dynamically display hour and current activity
@@ -14,35 +12,31 @@ $(function () {
     if (hour == dayjs().hour()) { block.attr("class","row time-block present") }
     if (hour > dayjs().hour()) { block.attr("class","row time-block future") }
     if (hour > 11) { var time = " PM";} else { var time = " AM";}
-    $("<div>",{ class: "col-2 col-md-1 hour text-center py-3"}).text((hour % 12) + time).appendTo(block);
-    $("<textarea>",{ class: "col-8 col-md-10 description", rows: "3"}).text("test").appendTo(block);
+    $("<div>",{ class: "col-2 col-md-1 hour text-center py-3"}).text((hour-((hour > 12)*12))+time).appendTo(block);
+    var textarea = $("<textarea>",{ class: "col-8 col-md-10 description", rows: "3"}).text(localStorage.getItem("hour-"+hour)).appendTo(block);
     var button = $("<button>",{ class: "btn saveBtn col-2 col-md-1"}).attr("aria-label","save").appendTo(block);
-    $("<i>",{ class: "fas fa-save"}).attr("aria-hidden","true").appendTo(button);
+    var save = $("<i>",{ class: "fas fa-save"}).attr("aria-hidden","true").appendTo(button);
 
     // create listener to save new activity if save button is clicked
+    save.click(function(){ localStorage.setItem("hour-" + hour, textarea.val())});
+
   }
 
+  $("#currentDay").text(dayjs().format("dddd MMMM D")); // display current time 
+
   var root = document.body.childNodes[1];
+  createHour(9, root);
+  createHour(10, root);
+  createHour(11, root);
+  createHour(12, root);
+  createHour(13, root);
+  createHour(14, root);
+  createHour(15, root);
+  createHour(16, root);
+  createHour(17, root);
+  createHour(18, root);
   createHour(19, root);
-  createHour(4, root);
+  createHour(20, root);
+  createHour(21, root);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
